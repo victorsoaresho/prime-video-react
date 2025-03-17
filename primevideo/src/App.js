786b5d1fe0b522f83c5d4ad6b9703b089d8ace94
Victor Soares <136899628+victorsoaresho@users.noreply.github.com>
2025-03-17 19:49:45 -0300

    feat: Finalizando a estrutura inicial

--- primevideo/src/App.js
+++ primevideo/src/App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>Up at app.js</Text>
-            <StatusBar style="auto"></StatusBar>
-        </View>
+        <>
+            <Home></Home>
+            <StatusBar style="auto"/>
+        </>
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
